fda3229d912ae1a9ede109c6b32522caa0a8aa0b
rodry <85464515+shodryi@users.noreply.github.com>
2023-10-23 14:34:18 -0300

    Update sticker-slap.js

--- plugins/sticker-slap.js
+++ plugins/sticker-slap.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn}) => {
 try {   
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://neko-love.xyz/api/v1/slap')
+let res = await fetch('https://api.otakugifs.xyz/gif?reaction=punch')
 let json = await res.json()
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio una bofetada a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
